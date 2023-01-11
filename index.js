ab20ce8c5a72ba7bd08a4e3a828e765a3c13e240
Damian Szmurlo <d.szmurlo@forbetterfuture.pl>
2023-01-11 02:54:44 +0100

    finish

---
+++ index.js
@@ -0,0 +1,57 @@
+const classifier = knnClassifier.create();
+const webcamElement = document.getElementById("webcam");
+
+let net;
+
+async function app() {
+  console.log("Loading mobilnet...");
+
+  net = await mobilenet.load();
+
+  console.log("Loaded model");
+
+  const webcam = await tf.data.webcam(webcamElement);
+
+  const addExample = async (classId) => {
+    const img = await webcam.capture();
+
+    const activation = net.infer(img, true);
+
+    classifier.addExample(activation, classId);
+
+    img.dispose();
+  };
+
+  document.getElementById("dog").addEventListener("click", () => addExample(0));
+  document.getElementById("cat").addEventListener("click", () => addExample(1));
+  document
+    .getElementById("snake")
+    .addEventListener("click", () => addExample(2));
+  document
+    .getElementById("horse")
+    .addEventListener("click", () => addExample(3));
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      const img = await webcam.capture();
+
+      const activation = net.infer(img, "conv_preds");
+
+      const result = await classifier.predictClass(activation);
+
+      const classes = ["Dog", "Cat", "Snake", "Horse"];
+
+      document.getElementById("console").innerText = `
+                Prediction: ${classes[result.label]} Probabilty: ${
+        result.confidences[result.label]
+      }
+            `;
+
+      img.dispose();
+    }
+
+    await tf.nextFrame();
+  }
+}
+
+app();
